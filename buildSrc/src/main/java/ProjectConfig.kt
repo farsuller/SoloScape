5e214180312807b537a98df41a89ca104e2415e6
farsuller <farsuller2020@gmail.com>
2023-12-23 14:14:27 +0800

    removed cached buildSrc build, then added archiveBaseName for outpul generated build, updates on Project Config

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -1,11 +1,17 @@
 object ProjectConfig {
-    const val appBuildOutput = "soloscape"
+    const val appFileName = "soloscape"
+    const val applicationId = "com.soloscape.compose"
+
+    const val namespace = "com.compose.soloscape"
+
     const val compileSdk = 34
     const val minSdk = 24
     const val targetSdk = 34
+
     const val extensionVersion = "1.4.6"
 
     const val versionCode = 3
+
     const val majorVersion = 1
     const val minorVersion = 0
     const val patchVersion = 0
